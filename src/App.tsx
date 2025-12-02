9fd8314c69f13a6e164ad713d66d2f0da9689b51
nina negahdar <bhm577@myy.haaga-helia.fi>
2025-12-02 14:57:26 +0200

    Navigaatio ja reitti lisätty kotisivulle App.tsx:ssä

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
-import { Link, Outlet } from "react-router";
+import { BrowserRouter as Router, Routes, Route, Link, Outlet } from 'react-router-dom';
 import { Menubar } from "./components/ui/menubar";
+import SurveyList from "./components/SurveyList";
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
         <Link to={"/surveys/:id/responses"}></Link>
       </nav>
       <Menubar />
+          <Routes>
+          <Route path="/" element={<SurveyList />} />
+        </Routes>
       <Outlet />
     </>
   );
